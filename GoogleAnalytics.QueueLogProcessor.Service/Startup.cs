456a6baa10d06608ba209165c87f04ff62abc8d7
Joshua Wieczorek <jwieczorek@amsinet.com>
2021-06-10 13:04:21 -0400

    initial commit

---
+++ GoogleAnalytics.QueueLogProcessor.Service/Startup.cs
@@ -0,0 +1,103 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using AAG.Global.Health;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using Bugsnag.AspNet.Core;
+using AAG.Global.Security;
+using GoogleAnalytics.Library.Services;
+using GoogleAnalytics.Library.Data;
+using GoogleAnalytics.Library.Common.HealthChecks;
+using GoogleAnalytics.Library.Common.MessageBroker;
+
+namespace GoogleAnalytics.QueueLogProcessor.Service
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+            => Configuration = configuration;
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add health checks.
+            services.AddHealthChecks()
+                .AddCheck<AppSettingsHealthCheck>("appSettings_check")
+                .AddCheck<AccountsDbHealthCheck>("accountsDb_check")
+                .AddCheck<GoogleAnalyticsDbHealthCheck>("googleAnalyticsDb_check");
+
+            // Add logging.
+            services.AddLogging(config =>
+            {
+                config.AddDebug();
+                config.AddConsole();
+            });
+
+            // Add Bug Snag.
+            services.AddBugsnag(cfg => {
+                cfg.ApiKey = Configuration["BugsnagApiKey"];
+                cfg.AppType = Configuration["AppType"];
+                cfg.AppVersion = Configuration["AppReleaseVersion"];
+                cfg.ReleaseStage = Configuration["AppReleaseStage"];
+            });
+
+            // Add other services here...
+            SecurityConfiguration securityConfiguration = new SecurityConfiguration();
+            Configuration.Bind("SecurityConfiguration", securityConfiguration);
+            services.AddSingleton<SecurityConfiguration>(securityConfiguration);
+            services.AddSingleton<CryptographyProvider>();
+            services.AddTransient<AppSettingsService>();
+            services.AddTransient<AccountsDbContext>();
+            services.AddTransient<IMessageBroker, RabbitMQMessageBroker>();
+            services.AddTransient<QueueLogProcessorService>();
+            services.AddTransient<QueueLogMessageBrokerReaderService>();
+        }
+
+
+        /// <summary>
+        /// Configuration.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(
+              IApplicationBuilder app
+            , IWebHostEnvironment env)
+        {
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    HealthCheckResponse response = new HealthCheckResponse
+                    {
+                        Status = report.Status.ToString(),
+                        Checks = report.Entries.Select(x => new HealthCheck
+                        {
+                            Component = x.Key,
+                            Status = x.Value.Status.ToString(),
+                            Description = x.Value.Description
+                        }),
+                        Duration = report.TotalDuration
+                    };
+
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+                }
+            });
+        }
+    }
+}
---
+++ GoogleAnalytics.QueueProcessor.Service/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using AAG.Global.Common;
+using NLog.Web;
+using System.Threading.Tasks;
+using GoogleAnalytics.Library.HostedServices;
+using System;
+using Newtonsoft.Json;
+
+namespace GoogleAnalytics.QueueProcessor.Service
+{
+    class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+            // Set current directory path and error file name.
+            var currentDirectoryPath = Directory.GetCurrentDirectory();
+            var startupErrorLogFile = $"_startup-errors-{DateTime.Now:yyyy-MM-dd}.log";
+
+            try
+            {
+                // Run the application.
+                await CreateHostBuilder(args)
+                    .RunConsoleAsync();
+            }
+            catch (Exception e)
+            {
+                var startupErrorLogFilePath = Path.Combine(currentDirectoryPath, startupErrorLogFile);
+                using StreamWriter streamWriter = new StreamWriter(startupErrorLogFilePath, append: true);
+                streamWriter.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss:fffffff}] ERROR | {e.Message}");
+                streamWriter.WriteLine($"Object: {JsonConvert.SerializeObject(e)}{Environment.NewLine}");
+            }
+        }
+
+
+        /// <summary>
+        /// Create the host builder.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+              .ConfigureWebHostDefaults(webBuilder =>
+              {
+                  webBuilder.UseStartup<Startup>();
+              })
+              .ConfigureLogging(logging =>
+              {
+                  logging.ClearProviders();
+                  logging.SetMinimumLevel(LogLevel.Information);
+              })
+              .ConfigureServices(services =>
+              {
+                  services.AddSingleton<ApplicationArguments>(new ApplicationArguments(args));
+                  services.AddHostedService<QueueProcessorHostedService>();
+              })
+             .ConfigureAppConfiguration(builder =>
+             {
+                 builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
+             })
+              .UseNLog();
+    }
+}
